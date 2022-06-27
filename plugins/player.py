ccd9e7f0890111c8e3c9e939fddc232b38799d49
ahmedtom <101574426+ahmedtom@users.noreply.github.com>
2022-06-28 01:46:51 +0200

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -129,7 +129,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "Tom_Player"
+        user.first_name = "Bikash_Player"
     usar = user
     wew = usar.id
     try:
