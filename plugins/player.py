089ba4eb2d4ba246636e7cb9293527830aaeaab8
ahmedtom <101574426+ahmedtom@users.noreply.github.com>
2022-09-27 02:14:29 +0200

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -111,7 +111,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
 
 @Client.on_message(
-    commandpro(["/play", ".play", "bgt", "!play", "play", "@", "#", "$", "تشغيل", "شغل", "توم", "حبيبه" ,"فينوم" ])
+    commandpro(["/play", ".play", "bgt", "!play", "play", "@", "#", "$", "تشغيل", "شغل", "توم", "صاصا" ,"فينوم" ])
     & filters.group
     & ~filters.edited
     & ~filters.forwarded
